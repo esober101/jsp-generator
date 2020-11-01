35eed2c23771809a82db00f26ed8c1b0cacd70e1
Eric Ober <ericscottober@gmail.com>
2020-10-31 22:55:29 -0400

    AddAdd

--- js/script.js
+++ js/script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+// Prompt and confirms for password attributes
+function generatePassword(){
+
+    // Variable declarations
+    var passwordLength = prompt("How many characters between 8-128 would you like your password?");
+    var lowerCase = confirm("Would you like lowercase characters?");
+    var upperCase = confirm("Would you like uppercase characters?");
+    var numericCase = confirm("Would you like numeric characters?");
+    var specialCase = confirm("Would you like special characters?");
+    var passwordOptions = [];
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
