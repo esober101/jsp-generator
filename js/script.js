f9b485b10b2974ac3239ae39982f47a48727783c
Eric Ober <ericscottober@gmail.com>
2020-10-31 22:59:22 -0400

    Added arrays.

--- js/script.js
+++ js/script.js
@@ -13,13 +13,19 @@ function writePassword() {
 // Prompt and confirms for password attributes
 function generatePassword(){
 
-    // Variable declarations
-    var passwordLength = prompt("How many characters between 8-128 would you like your password?");
-    var lowerCase = confirm("Would you like lowercase characters?");
-    var upperCase = confirm("Would you like uppercase characters?");
-    var numericCase = confirm("Would you like numeric characters?");
-    var specialCase = confirm("Would you like special characters?");
-    var passwordOptions = [];
+// Variable declarations
+var passwordLength = prompt("How many characters between 8-128 would you like your password?");
+var lowerCase = confirm("Would you like lowercase characters?");
+var upperCase = confirm("Would you like uppercase characters?");
+var numericCase = confirm("Would you like numeric characters?");
+var specialCase = confirm("Would you like special characters?");
+var passwordOptions = [];
+
+// Arrays for lowercase, uppercase, numeric, and special characters
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*","(",")","+"];
 
 }
 
